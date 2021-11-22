fcd91616ed80d822a35fe1ba8e13762b77c0b7bf
liuyang <1773587692@qq.com>
2021-11-22 15:13:57 +0800

    something

---
+++ nodejs/web/koa-router2/app.js
@@ -0,0 +1,52 @@
+const Koa = require("koa");
+
+const router = require("koa-router")();
+
+const bodyParser = require("koa-bodyparser");
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
+});
+
+// router.get("/hello/:name", async (ctx, next) => {
+//   var name = ctx.params.name;
+//   ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+// });
+
+// router.get("/", async (ctx, next) => {
+//   ctx.response.body = "<h1>Index</h1>";
+// });
+
+router.get("/", async (ctx, next) => {
+  ctx.response.body = `<h1>Index</h1>
+    <form action="/signin" method="post">
+      <p>Name: <input name="name" value="koa"></p>
+      <p>Password: <input name="password" type="password"></p>
+      <p><input type="submit" value="Submit"></p>
+  `;
+});
+
+router.post("/signin", async (ctx, next) => {
+  var name = ctx.request.body.name || "";
+  var password = ctx.request.body.password || "";
+  console.log(`name: ${name} password: ${password}`);
+  if(name === "liuyang" && password === "123456"){
+    ctx.response.body = `<h1>Welcome,${name}</h1>`
+  }else {
+    ctx.response.body = `<h1>Login failed!</h1>
+      <p><a href="/">Sign In</a></p>
+    `
+  }
+});
+
+//注意位置（middleware按顺序执行） 要放到   app.use(router.routes()) 前面
+app.use(bodyParser());
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+console.log("app started at port http://127.0.0.1:3000...");

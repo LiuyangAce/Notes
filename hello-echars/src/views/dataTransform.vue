daa5848737003503f13998c1144e425e5212b8e2
liuyang <1773587692@qq.com>
2022-01-04 09:53:22 +0800

    echars数据转换

---
+++ hello-echars/src/views/dataTransform.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="echarsContain"></div>
+</template>
+<script>
+import *as echarts from 'echarts'
+export default {
+  data() {
+    return {
+      option: {
+        xAxis: {
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {},
+        series: [
+          {
+            type: 'bar',
+            data: [23, 24, 18, 25, 27, 28, 25]
+          }
+        ]
+      }
+    }
+  },
+  created() {
+    let a = echarts.init(document.getElementById('echarsContain'))
+    console.log(a)
+  },
+  methods: {
+
+  }
+}
+</script>

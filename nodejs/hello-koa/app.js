92800f363431a5ba75e6ac161b811bb4ccb2dcb6
liuyang <1773587692@qq.com>
2022-01-16 10:25:20 +0800

    koa-generator

--- nodejs/hello-koa/app.js
+++ nodejs/hello-koa/app.js
@@ -19,7 +19,7 @@ app.use(async (ctx, next) => {
 app.use(async (ctx, next) => {
   await next();
   ctx.response.type = 'text/html'
-  ctx.response.body = '<h1>Hello, koa2!</h1>'
+  ctx.response.body = '<h1>Hello, koa2!!!</h1>'
 })
 
 app.listen(3000)

f6fce4b37503207c6738b1af43bf29056e2f75d2
liuyang <1773587692@qq.com>
2023-07-28 14:30:11 +0800

    feat: ts-interface1

---
+++ new-ts/test/test.ts
@@ -0,0 +1,26 @@
+let d :boolean = true
+
+let d1: boolean = new Boolean(1)
+
+let d2: boolean = Boolean(1)
+
+let str: string = '123'
+let str1: string = String('123')
+
+let arr: number[] = [1,2,3]
+let arr1: Array<number> = [1,2,3]
+
+let tuple: [number, boolean] = [1, true]
+
+let notSure123: unknown = 123
+notSure123 = true
+
+let anyVal: any = 234
+anyVal = true
+
+let voidVal: void = undefined
+let voidVal1: void = null
+
+
+let nullVal: number = null
+let nullVal1: number = undefined
